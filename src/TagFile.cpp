0a46909c3d67a544a1ee97b191f1988faee2de31
skyostil <skyostil>
2004-02-26 22:51:48 +0000

    Tagfile myos.

---
+++ src/TagFile.cpp
@@ -0,0 +1,102 @@
+/***************************************************************************
+ *   Copyright (C) 2004 by Sami Kyöstilä                                   *
+ *   skyostil@kempele.fi                                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#include "TagFile.h"
+#include "Config.h"
+
+#ifdef EPOC
+#include <ezlib.h>
+#else
+#include <zlib.h>
+#endif
+
+TagFile::TagFile(const char *name)
+{
+	tag.id = 0;
+	tag.flags = 0;
+	tag.size = 0;
+	tag.uncompressedSize = 0;
+	file = fopen(name, "rb");
+}
+
+TagFile::~TagFile()
+{
+	if (file)
+	{
+		fclose(file);
+		file = 0;
+	}
+}
+
+int TagFile::readTag()
+{
+	if (file)
+	{
+		unsigned int header;
+		if (fread(&header, sizeof(header), 1, file) != 1)
+		{
+			tag.size = tag.uncompressedSize = 0;
+			return -1;
+		}
+		
+		tag.flags = (header>>28);
+		tag.id = (header>>24) & 0xf;
+		tag.size = header & 0x00ffffff;
+		
+		if (tag.flags & FlagCompressed)
+		{
+			fread(&tag.uncompressedSize, sizeof(tag.uncompressedSize), 1, file);
+		}
+		
+		return tag.id;
+	}
+	return -1;
+}
+
+unsigned int TagFile::getDataSize()
+{
+	if (tag.flags & FlagCompressed)
+	{
+		return tag.uncompressedSize;
+	}
+	return tag.size;
+}
+
+unsigned int TagFile::readData(unsigned char *data, unsigned int size)
+{
+	if (!file || tag.size == 0)
+		return 0;
+	
+	if (size < getDataSize())
+		return 0;
+
+	if (tag.flags & FlagCompressed)
+	{
+		unsigned char *compressed = new unsigned char[tag.size];
+		unsigned long len = tag.uncompressedSize;
+		fread(compressed, tag.size, 1, file);
+		
+		bool success = (uncompress((Bytef*)data, &len, compressed, tag.size) == Z_OK);
+		
+		delete[] compressed;
+		return len;
+	}
+	
+	return fread(&data, tag.size, 1, file);
+}
